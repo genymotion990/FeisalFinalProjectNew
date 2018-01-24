b3b47d9a6ace208ba8f4ec7d0a588af9701bce44
samar2705 <samarballan77@gmail.com>
2018-01-24 09:02:17 +0200

    new

--- app/src/main/java/com/example/hp1/myapplication1/MapsActivity2.java
+++ app/src/main/java/com/example/hp1/myapplication1/MapsActivity2.java
@@ -47,21 +47,64 @@ public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallbac
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        LatLng america = new LatLng(-340, 151);
 
-        LatLng startLatLng = new LatLng(30.707104, 76.690749);
-        LatLng endLatLng = new LatLng(30.721419, 76.730017);
+        LatLng harinaftali = new LatLng(33.239051, 35.571829);
+        LatLng marom_hagolan = new LatLng(33.136622, 35.769492);
+        LatLng katzrin = new LatLng(32.991109, 35.682730);
+        LatLng bat_yaar = new LatLng(32.996329, 35.519112);
+        LatLng alon_hagalil = new LatLng(32.763976, 35.221433);
+        LatLng hasolilim = new LatLng(32.750992, 35.242269);
+        LatLng shemshit = new LatLng(32.745535, 35.242823);
+        LatLng gevaat_ela = new LatLng(32.723106, 35.243902);
+        LatLng temrat = new LatLng(32.703497, 35.214996);
+        LatLng kfar_hahoresh = new LatLng(32.696851, 35.236365);
+        LatLng nahal_hashofet = new LatLng(32.636967, 35.110124);
+
+        LatLng sovev_ofakim = new LatLng(31.338205, 34.651218);
 
 
-        List<LatLng> list = new ArrayList<LatLng>();
-        list.add(0,america);
-        list.add(1,sydney);
 
-        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
-        drawPolyLineOnMap(list);
+
+
+
+
+
+
+
+        googleMap.addMarker(new MarkerOptions().position(harinaftali).title("סינגיל הרי נפתלי"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(harinaftali));
+        googleMap.addMarker(new MarkerOptions().position(marom_hagolan).title("סינגיל מרום הגולן"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(marom_hagolan));
+        googleMap.addMarker(new MarkerOptions().position(katzrin).title(" שביל אופניים קצרין"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(katzrin));
+        googleMap.addMarker(new MarkerOptions().position(bat_yaar).title(" שביל אופניים בת יער"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(bat_yaar));
+        googleMap.addMarker(new MarkerOptions().position(alon_hagalil).title(" סינגיל אלון הגליל"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(alon_hagalil));
+        googleMap.addMarker(new MarkerOptions().position(hasolilim).title(" סינגיל סוללים"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(hasolilim));
+        googleMap.addMarker(new MarkerOptions().position(shemshit).title(" סינגיל שמשית"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(shemshit));
+        googleMap.addMarker(new MarkerOptions().position(gevaat_ela).title(" סינגיל בעת אלה"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(gevaat_ela));
+        googleMap.addMarker(new MarkerOptions().position(temrat).title(" סינגיל תמרת"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(temrat));
+        googleMap.addMarker(new MarkerOptions().position(kfar_hahoresh).title(" סינגיל כפר החורש"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(kfar_hahoresh));
+        googleMap.addMarker(new MarkerOptions().position(nahal_hashofet).title(" סינגיל נחל השופט"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(nahal_hashofet));
+        googleMap.addMarker(new MarkerOptions().position(sovev_ofakim).title(" סינגיל סובב אופקים"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sovev_ofakim));
+
+
+
+
+
+
+
+
+
 
     }
     // Draw polyline on map
